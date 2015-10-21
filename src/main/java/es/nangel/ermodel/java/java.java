06805c9a696545869d059ec8db9cb2e8a79340b4
Ángel Luis López Monterroso <angel9484@gmail.com>
2015-10-21 22:52:18 +0200

    ERModel does the same thing as a CASE tool. It will take one yml, json, xml, etc, parse it to a Tree structure and then creates the class/structure files to the desired language of the Entity Relationship. Is a plugin based project. Initially only parses yml and outputs to java (plain, no hibernate) The extensibility is creating projects using one interface to parse, and other interface to create language-class-file. -- Created initial structure. Created skeleton of processing.

---
+++ src/main/java/es/nangel/ermodel/java/java.java
@@ -0,0 +1,12 @@
+package es.nangel.ermodel.java;
+
+import es.nangel.ermodel.model.Entity;
+
+/**
+ * Created by nangel on 21/10/15.
+ */
+public class java implements Entity {
+    public void process(String s) {
+        System.out.println(s);
+    }
+}
